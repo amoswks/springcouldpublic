d2ae15abc57f355668d15ede5975da97bde93abc
wangke <919964583@qq.com>
2019-06-15 23:43:35 +0800

    完善pageHelper

--- eureka-client/src/main/java/com/cloud/eurekaclient/common/contoller/BaseContrller.java
+++ eureka-client/src/main/java/com/cloud/eurekaclient/common/contoller/BaseContrller.java
@@ -1,13 +1,34 @@
 package com.cloud.eurekaclient.common.contoller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.cloud.eurekaclient.common.dto.BaseResponse;
+import com.cloud.eurekaclient.common.dto.PageResponse;
+import com.cloud.eurekaclient.common.service.BaseService;
+import com.github.pagehelper.PageHelper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-public abstract class BaseContrller<E,T> {
-    protected Logger log = LoggerFactory.getLogger(this.getClass());
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+@Slf4j
+public abstract class BaseContrller<E extends BaseService<T>, T> {
+
+    //protected Logger log = LoggerFactory.getLogger(this.getClass());
     @Autowired
-    protected  T service;
+    protected E service;
 
+    @ResponseBody
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public BaseResponse<List<T>> list(HttpServletRequest request, T selectOption,
+                                      @RequestParam(value = "pageSize",defaultValue = "15", required = false) Integer pageSize
+                                     , @RequestParam(value = "pageNum",defaultValue = "1",required = false) Integer pageNum) {
+
+        if (pageNum != null && pageSize != null){
+            PageHelper.startPage(pageNum,pageSize);
+        }
+        //PageInfo
+        List<T> result = service.selectList(selectOption);
+        return PageResponse.successPage(result);
+    }
 
 }
--- eureka-client/src/main/java/com/cloud/eurekaclient/common/service/impl/BaseServiceImpl.java
+++ eureka-client/src/main/java/com/cloud/eurekaclient/common/service/impl/BaseServiceImpl.java
@@ -1,7 +1,27 @@
 package com.cloud.eurekaclient.common.service.impl;
 
 import com.cloud.eurekaclient.common.service.BaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import tk.mybatis.mapper.common.Mapper;
 
-public abstract class BaseServiceImpl implements BaseService {
+import java.util.List;
 
+public abstract class BaseServiceImpl<M extends Mapper<T>,T> implements BaseService<T> {
+    @Autowired
+    public M mapper;
+
+    @Override
+    public T selectOne(T entity) {
+        return mapper.selectOne(entity);
+    }
+
+    @Override
+    public T selectById(Object id) {
+        return mapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public List<T> selectList(T entity) {
+        return mapper.select(entity);
+    }
 }
